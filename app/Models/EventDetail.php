38ce8ee6df576cee633e987ea7f9472f92dc8128
David <muhammaddavid81@gmail.com>
2025-12-12 08:59:06 +0700

    fix create event

--- app/Models/EventDetail.php
+++ app/Models/EventDetail.php
@@ -14,8 +14,8 @@ class EventDetail extends Model
     protected $casts = [
         'event_detail_id' => 'integer',
         'event_id' => 'integer',
-        'register_open_date' => 'datetime',
-        'register_closed_date' => 'datetime',
+        'register_open_date' => 'date',
+        'register_closed_date' => 'date',
         'register_status' => 'boolean',
         'total_participant' => 'integer',
         'date' => 'datetime',
@@ -34,6 +34,7 @@ class EventDetail extends Model
         "register_closed_date",
         "register_status",
         "total_participant",
+        "date",
         "event_handler",
         "cost",
         "paid_status"
